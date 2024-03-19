91b09a6a67e04a56cb60488c63d0ca19a6ae22d9
Gary Patay <gary.patay@icloud.com>
2024-03-19 10:17:12 +0100

    Added fixes for Onboarding steps

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,164 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+import AppButton from '../compontents/Button';
+import styles from '../Styles';
+
+export default function OnboardingScreen({ navigation }) {
+    const [currentStep, setCurrentStep] = useState(0);
+    const swiperRef = useRef(null);
+
+    const onboardingData = [
+        {
+            image: require('../assets/img/onboarding-welcome.png'),
+            title: 'Welcome to Maitri',
+            text: 'A new way to ask for help.',
+        },
+        {
+            image: require('../assets/img/onboarding-build-tribe.png'),
+            title: 'Build your tribe',
+            text: 'Engage your community and create a support network you can lean on.',
+        },
+        {
+            image: require('../assets/img/onboarding-share-load.png'),
+            title: 'Share the load',
+            text: 'Set up tasks to make any situation of need easier and more manageable.',
+        },
+    ];
+
+    const handleSkip = () => {
+        setCurrentStep(onboardingData.length - 1);
+        swiperRef.current.scrollBy(1);
+    };
+
+    useEffect(() => {
+        if (swiperRef.current && currentStep !== swiperRef.current.state.index) {
+            swiperRef.current.scrollBy(currentStep - swiperRef.current.state.index);
+        }
+    }, [currentStep]);
+
+    return (
+        <View style={{ flex: 1}}>
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                index={currentStep}
+                onIndexChanged={(index) => setCurrentStep(index)}
+                showsButtons={false}
+                showsPagination={false}
+            >
+                {onboardingData.map((step, index) => (
+                    <View key={index} style={styles.container}>
+                        <Image source={step.image} style={stylesOnboard.onboardingImg} />
+                        <View style={stylesOnboard.onboardingTextContainer}>
+                            <Text style={stylesOnboard.title}>{step.title}</Text>
+                            <Text style={stylesOnboard.text}>{step.text}</Text>
+                        </View>
+                        {index < onboardingData.length - 1 && (
+                            <TouchableOpacity onPress={handleSkip} style={stylesOnboard.skipButton}>
+                                <Text style={stylesOnboard.skipButtonText}>Skip</Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                ))}
+            </Swiper>
+            {currentStep === onboardingData.length - 1 && (
+                <View style={stylesOnboard.onboardingButtonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <AppButton
+                            onPress={() => navigation.navigate('Login')}
+                            title="Create Account"
+                            buttonStyle={stylesOnboard.buttonStyle}
+                        />
+                    </View>
+                    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                        <Text style={stylesOnboard.haveAccountText}>Already have an account? <Text style={stylesOnboard.loginText}>Log in</Text></Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+            <View style={stylesOnboard.dotsContainer}>
+                {onboardingData.map((_, index) => (
+                    <View
+                        key={index}
+                        style={[stylesOnboard.dot, index === currentStep && stylesOnboard.activeDot]}
+                    />
+                ))}
+            </View>
+        </View>
+    );
+}
+
+const stylesOnboard = StyleSheet.create({
+    onboardingImg: {
+        resizeMode: 'contain',
+        width: 350,
+        height: 290,
+        marginTop: -140,
+        marginBottom: 10,
+    },
+    dotsContainer: {
+        width: '100%',
+        position: 'absolute',
+        bottom: '28%',
+        left: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    dot: {
+        width: 5,
+        height: 5,
+        borderRadius: 5,
+        backgroundColor: '#A3A3A3',
+        marginHorizontal: 2,
+    },
+    activeDot: {
+        backgroundColor: '#000',
+        width: 20,
+    },
+    onboardingTextContainer: {
+        alignItems: 'center',
+        gap: 10,
+        paddingHorizontal: 30,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '500',
+        fontFamily: 'poppins-medium',
+    },
+    text: {
+        textAlign: 'center',
+        color: '#7A7A7A',
+        fontSize: 14,
+    },
+    skipButton: {
+        position: 'absolute',
+        top: 70,
+        right: 25,
+    },
+    skipButtonText: {
+        fontWeight: '600',
+        textDecorationLine: 'underline',
+        fontSize: 14,
+        padding: 5,
+    },
+    onboardingButtonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        left: 0,
+        width: '100%',
+        alignItems: 'center',
+        gap: 30,
+    },
+    buttonStyle: {
+        width: '100%',
+    },
+    haveAccountText: {
+        textDecorationLine: 'underline',
+        fontSize: 15,
+    },
+    loginText: {
+        fontWeight: '600',
+        fontFamily: 'poppins-semibold',
+    }
+});
