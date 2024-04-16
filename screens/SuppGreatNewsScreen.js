ed0d9dfb5c2fc69f798d23ddac841541b10f5d39
pestiantal <pesti.antal1@gmail.com>
2024-04-16 23:44:49 +0200

    Update SuppGreatNewsScreen.js

--- screens/SuppGreatNewsScreen.js
+++ screens/SuppGreatNewsScreen.js
@@ -12,9 +12,8 @@ const SuppExistingScreen = () => {
   return (
     <SafeAreaView style={[styles.safeArea, stylesSuppGN.safeArea]}>
       <View style={styles.contentContainer}>
-        <View style={stylesSuppGN.innerContainer}>
           <View style={stylesSuppGN.topBar}>
-            <TouchableOpacity style={stylesSuppGN.closeIconWrapper} onPress={() => console.log('Close')}>
+            <TouchableOpacity onPress={() => console.log('Close')}>
               <CloseIcon width={19} height={19} color={'#000'} />
             </TouchableOpacity>
           </View>
@@ -41,14 +40,9 @@ const SuppExistingScreen = () => {
             <AppButton
               title="I'm Ready!"
               onPress={() => console.log('Button pressed!')}
-              buttonStyle={stylesSuppGN.button}
             />
           </View>
-          <TouchableOpacity style={stylesSuppGN.notYouTextWrapper} onPress={() => console.log('Back')}>
-            <Text style={stylesSuppGN.notYouText}>Not You? Click Here</Text>
-          </TouchableOpacity>
         </View>
-      </View>
     </SafeAreaView>
   );
 };
@@ -57,13 +51,13 @@ const stylesSuppGN = StyleSheet.create({
   safeArea: {
     backgroundColor: '#E5F5E3',
   },
-  innerContainer: {
-    position: 'relative',
-    flex: 1,
+  topBar: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 20,
   },
   illustrationWrapper: {
-    position: 'absolute',
-    top: 358,
+    paddingTop: 40,
     alignSelf: 'center',
   },
   illustration: {
@@ -72,84 +66,64 @@ const stylesSuppGN = StyleSheet.create({
     height: 178,
   },
   headerWrapper: {
-    position: 'absolute',
-    top: 154,
+    paddingTop: 60,
     alignSelf: 'center',
   },
   bodyWrapper: {
-    position: 'absolute',
-    top: 223,
+    paddingTop: 30,
     alignSelf: 'center',
   },
   title: {
     fontSize: 25,
     fontFamily: 'poppins-medium',
-    color: '#55884C'
   },
   text: {
     color: '#000000',
     fontSize: 14,
     fontFamily: 'poppins-regular',
-    paddingHorizontal: 60, // add vertical padding
-    textAlign: 'center', // align text to center
-  },
-  buttonContainer: {
-    position: 'absolute',
-    top: 591,
-    width: '100%',
-    paddingHorizontal: 15,
+    paddingHorizontal: 60, 
+    textAlign: 'center', 
   },
   boldText: {
     fontFamily: 'poppins-bold',
   },
-  notYouTextWrapper: {
-    position: 'absolute',
-    top: 750,
-    alignSelf: 'center',
-  },
-  notYouText: {
-    color: '#000000',
-    fontSize: 12,
-    fontFamily: 'poppins-regular',
-    textDecorationLine: 'underline',
+  buttonContainer:{
+    paddingTop: 80,
+    width: '100%',
+    paddingHorizontal: 15,
   },
   star1: {
     position: 'absolute',
     left: 240,
-    top: 93,
-    width: 22, // adjust as needed
-    height: 22, // adjust as needed
+    top: 43,
+    width: 22,
+    height: 22,
     transform: [{ rotate: '0deg' }],
   },
   star2: {
     position: 'absolute',
     left: 11,
-    top: 186,
-    width: 28, // adjust as needed
-    height: 28, // adjust as needed
+    top: 136,
+    width: 28, 
+    height: 28, 
     transform: [{ rotate: '-21deg' }],
   },
   star3: {
     position: 'absolute',
     left: 305,
-    top: 349,
-    width: 30, // adjust as needed
-    height: 30, // adjust as needed
+    top: 299,
+    width: 30,
+    height: 30,
     transform: [{ rotate: '-22deg' }],
   },
   star4: {
     position: 'absolute',
     left: -20,
-    top: 555,
-    width: 50, // adjust as needed
-    height: 50, // adjust as needed
+    top: 465,
+    width: 50,
+    height: 50,
     transform: [{ rotate: '85deg' }],
-  },
-  topBar: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    marginTop: 30,
-  },
+  }
 });
 
 export default SuppExistingScreen;
