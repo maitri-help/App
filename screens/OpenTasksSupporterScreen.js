465b367d4c4157ab1af7e496423b327aa851dafb
Kitti Rózsavölgyi <kitti.rozsavolgyi@cubicfox.com>
2024-05-22 09:09:10 +0200

    open tasks filtering

--- screens/OpenTasksSupporterScreen.js
+++ screens/OpenTasksSupporterScreen.js
@@ -9,6 +9,13 @@ import TaskDetailsModal from '../components/plusModalSteps/TaskDetailsModal';
 import TaskFilterModal from '../components/plusModalSteps/TaskFilterModal';
 import Button from '../components/Button';
 
+const TIME_FILTER_HOURS = {
+    Morning: { start: 6, end: 12 },
+    Afternoon: { start: 12, end: 17 },
+    Evening: { start: 17, end: 22 },
+    Night: { start: 22, end: 6 },
+};
+
 export default function OpenTasksSupporterScreen({ navigation }) {
     const [taskModalVisible, setTaskModalVisible] = useState(false);
     const [selectedTask, setSelectedTask] = useState(null);
@@ -16,6 +23,8 @@ export default function OpenTasksSupporterScreen({ navigation }) {
     const [TimeFilterList, setTimeFilterList] = useState([]);
     const [TypeFilterList, setTypeFilterList] = useState([]);
     const [tasks, setTasks] = useState([]);
+    const [filteredTasks, setFilteredTasks] = useState([]);
+    const [taskRemoval, setTaskRemoval] = useState(0);
     const [leadId, setLeadId] = useState('');
 
     const overlayOpacity = useRef(new Animated.Value(0)).current;
@@ -47,6 +56,7 @@ export default function OpenTasksSupporterScreen({ navigation }) {
                 const tasksResponse = await getLeadUser(accessToken);
 
                 setTasks(tasksResponse.data[0].tasks);
+                setFilteredTasks(tasksResponse.data[0].tasks);
             } else {
                 console.error('No user data found or leadId not available');
             }
@@ -63,7 +73,12 @@ export default function OpenTasksSupporterScreen({ navigation }) {
 
     const handleRemoveFilter = (filter, setSelectedFilters) => {
         setSelectedFilters(selectedFilters => selectedFilters.filter(f => f !== filter));
+        setTaskRemoval(taskRemoval === 0 ? 1 : 0);
     };
+
+    useEffect(() => {
+        filterTasks();
+    }, [taskRemoval]);
 
     useEffect(() => {
         if (taskModalVisible) {
@@ -90,20 +105,107 @@ export default function OpenTasksSupporterScreen({ navigation }) {
     };
 
     const handleFilter = () => {
-        console.log("filter");
-        console.log(TimeFilterList);
-        console.log(TypeFilterList);
         setIsFilterOpen(true);
     };
 
     const handleFilterClose = () => {
+        filterTasks();
         setIsFilterOpen(false);
+    };
+
+    const isWithinTimeframe = (date, range) => {
+        const hours = date.getHours();
+
+        if (range.start <= range.end) {
+            return hours >= range.start && hours < range.end;
+        } else {
+            // Handles the wrap-around for night timeframe
+            return hours >= range.start || hours < range.end;
+        };
+    };
+
+    const isWeekend = (date) => {
+        const day = date.getDay();
+        return day === 0 || day === 6;
+    };
+
+    const isWeekday = (date) => {
+        const day = date.getDay();
+        return day >= 1 && day <= 5;
+    };
+
+    const filterTasks = () => {
+        // Reset list if no filters are selected
+        if (TimeFilterList.length === 0 && TypeFilterList.length === 0) {
+            setFilteredTasks(tasks);
+            return;
+        }
+
+        // Filter tasks based on selected filters
+        const filtered = tasks.filter(task => {
+            const start = new Date(task.startDateTime);
+            const end = new Date(task.endDateTime);
+
+            const duration = (end - start) / (1000 * 60 * 60 * 24);
+
+            let timeMatch = false;
+            let typeMatch = false;
+
+            // Time filters
+            if (TimeFilterList.length > 0) {
+                // Separate timeframe and weekday filters
+                const dayFilters = TimeFilterList.filter(filter => filter === 'Weekend' || filter === 'Weekday');
+                const frameFilters = TimeFilterList.filter(filter => !(filter === 'Weekend' || filter === 'Weekday'));
+
+                const frameMatch = frameFilters.some(filter => {
+                    if (filter in TIME_FILTER_HOURS) {
+                        return duration <= 1 && (isWithinTimeframe(start, TIME_FILTER_HOURS[filter]) || isWithinTimeframe(end, TIME_FILTER_HOURS[filter]));
+                    }
+                    return false;
+                });
+
+                const dayMatch = dayFilters.some(filter => {
+                    if (filter === 'Weekend') {
+                        return isWeekend(start) || isWeekend(end);
+                    } else if (filter === 'Weekday') {
+                        return isWeekday(start) || isWeekday(end);
+                    }
+                    return false;
+                });
+
+                // Combine timeframe and weekday filter matches
+                if (frameFilters.length > 0 && dayFilters.length > 0) {
+                    timeMatch = frameMatch && dayMatch;
+                } else if (frameFilters.length > 0) {
+                    timeMatch = frameMatch;
+                } else if (dayFilters.length > 0) {
+                    timeMatch = dayMatch;
+                }
+            }
+
+            // Type/category filters
+            if (TypeFilterList.length > 0) {
+                typeMatch = TypeFilterList.includes(task.category);
+            }
+
+            // Combine time and type filter matches
+            if (TimeFilterList.length > 0 && TypeFilterList.length > 0) {
+                return timeMatch && typeMatch;
+            } else if (TimeFilterList.length > 0) {
+                return timeMatch;
+            } else if (TypeFilterList.length > 0) {
+                return typeMatch;
+            }
+            return false;
+        });
+
+        setFilteredTasks(filtered);
     };
 
     const renderTasks = (tasks) => {
         const openTasks = tasks.filter(task => task.status === 'undone' && !task.assignedUserId);
 
-        if (openTasks.length === 0) {
+        /* if (openTasks.length === 0) {
             return (
                 <View style={stylesSuppOT.tasksContainer}>
                     <ScrollView contentContainerStyle={stylesSuppOT.tasksScrollEmpty}>
@@ -124,7 +226,7 @@ export default function OpenTasksSupporterScreen({ navigation }) {
                     </ScrollView>
                 </View>
             );
-        }
+        } */
 
         return (
             <>
@@ -162,24 +264,58 @@ export default function OpenTasksSupporterScreen({ navigation }) {
                         />
                     ))}
                 </View>
+                
+                {openTasks.length === 0 ? (
+                    <ScrollView contentContainerStyle={stylesSuppOT.tasksScrollEmpty}>
+                        <View style={[styles.contentContainer, stylesSuppOT.tasksEmpty]}>
+                            <View style={stylesSuppOT.tasksTop}>
+                                {TimeFilterList.length > 0 || TypeFilterList.length > 0 ? (
+                                    <>
+                                        <Text style={[styles.text, stylesSuppOT.tasksDescription, { marginBottom: 30 }]}>
+                                            No open tasks with the selected filters.
+                                        </Text>
+                                        <Text style={[styles.text, stylesSuppOT.tasksDescription, { marginBottom: 60, paddingHorizontal: 30 }]}>
+                                            Try removing some filters!
+                                        </Text>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Text style={[styles.text, stylesSuppOT.tasksDescription, { marginBottom: 30 }]}>
+                                            No open tasks yet.
+                                        </Text>
+                                        <Text style={[styles.text, stylesSuppOT.tasksDescription, { marginBottom: 60, paddingHorizontal: 30 }]}>
+                                            Check back in later!
+                                        </Text>
+                                    </>
+                                )}
+                                
+                                <Image
+                                    source={require('../assets/img/mimi-illustration.png')}
+                                    style={stylesSuppOT.rightImageStyle}
+                                />
+                            </View>
+                        </View>
+                    </ScrollView>
+                ) : (
+                    <ScrollView contentContainerStyle={stylesSuppOT.tasksScroll}>
+                        {openTasks.map((task) => (
+                            <OpenTask
+                                key={task.taskId}
+                                task={task}
+                                title={task.title}
+                                firstName={task.assignee ? task.assignee.firstName : ''}
+                                lastName={task.assignee ? task.assignee.lastName : ''}
+                                startTime={task.startDateTime}
+                                endTime={task.endDateTime}
+                                emoji={task.assignee ? task.assignee.emoji : ''}
+                                color={task.assignee ? task.assignee.color : ''}
+                                taskModal={() => setTaskModalVisible(true)}
+                                onTaskItemClick={handleTaskItemClick}
+                            />
+                        ))}
+                    </ScrollView>
+                )}
 
-                <ScrollView contentContainerStyle={stylesSuppOT.tasksScroll}>
-                    {openTasks.map((task) => (
-                        <OpenTask
-                            key={task.taskId}
-                            task={task}
-                            title={task.title}
-                            firstName={task.assignee ? task.assignee.firstName : ''}
-                            lastName={task.assignee ? task.assignee.lastName : ''}
-                            startTime={task.startDateTime}
-                            endTime={task.endDateTime}
-                            emoji={task.assignee ? task.assignee.emoji : ''}
-                            color={task.assignee ? task.assignee.color : ''}
-                            taskModal={() => setTaskModalVisible(true)}
-                            onTaskItemClick={handleTaskItemClick}
-                        />
-                    ))}
-                </ScrollView>
             </>
         );
     };
@@ -197,7 +333,7 @@ export default function OpenTasksSupporterScreen({ navigation }) {
                     </TouchableOpacity>
                 </View>
                 <View style={stylesSuppOT.tabsContentContainer}>
-                    {renderTasks(tasks)}
+                    {renderTasks(filteredTasks)}
                 </View>
 
                 {(isFilterOpen) && (
