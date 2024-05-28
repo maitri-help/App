3b7526ae27271db708a0e503cb5ef535da53c7a8
Roland Füst <roland.fust@cubicfox.com>
2024-05-28 07:59:23 +0200

    feat: support screen loading screen

--- screens/ProfileSupporterScreen.js
+++ screens/ProfileSupporterScreen.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Animated, Linking } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Animated,
+    Linking,
+    ActivityIndicator
+} from 'react-native';
 import styles from '../Styles';
-import { checkAuthentication, clearUserData, clearAccessToken } from '../authStorage';
+import {
+    checkAuthentication,
+    clearUserData,
+    clearAccessToken
+} from '../authStorage';
 import LogoutModal from '../components/profileModals/LogoutModal';
 import DeleteModal from '../components/profileModals/DeleteModal';
 import { Platform } from 'react-native';
@@ -11,19 +24,23 @@ export default function ProfileSupporterScreen({ navigation }) {
     const [logoutModalVisible, setLogoutModalVisible] = useState(false);
     const [deleteModalVisible, setDeleteModalVisible] = useState(false);
     const overlayOpacity = useRef(new Animated.Value(0)).current;
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchUserData() {
             try {
+                setIsLoading(true);
                 const userData = await checkAuthentication();
                 if (userData) {
                     setUserData(userData);
                 }
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching user data:', error);
                 clearUserData();
                 clearAccessToken();
                 navigation.navigate('Login');
+                setIsLoading(false);
             }
         }
         fetchUserData();
@@ -40,7 +57,7 @@ export default function ProfileSupporterScreen({ navigation }) {
             navigation.reset({
                 index: 0,
                 routes: [{ name: 'Login' }]
-            })
+            });
         } catch (error) {
             console.error('Error logging out:', error);
         }
@@ -51,13 +68,13 @@ export default function ProfileSupporterScreen({ navigation }) {
             Animated.timing(overlayOpacity, {
                 toValue: 1,
                 duration: 300,
-                useNativeDriver: true,
+                useNativeDriver: true
             }).start();
         } else {
             Animated.timing(overlayOpacity, {
                 toValue: 0,
                 duration: 300,
-                useNativeDriver: true,
+                useNativeDriver: true
             }).start();
         }
     }, [logoutModalVisible || deleteModalVisible]);
@@ -69,36 +86,92 @@ export default function ProfileSupporterScreen({ navigation }) {
                     <Text style={stylesProfile.greetingsText}>My Profile</Text>
                 </View>
                 <View style={[styles.container, stylesProfile.container]}>
-                    {userData && (
+                    {isLoading ? (
+                        <View
+                            style={{
+                                minHeight: 135,
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <ActivityIndicator size="large" />
+                        </View>
+                    ) : (
                         <View style={stylesProfile.topContent}>
                             <View style={{ paddingBottom: 20 }}>
-                                <View style={[
-                                    stylesProfile.selectedEmojiItem, { borderColor: userData.color, }
-                                ]}>
-                                    <Text style={stylesProfile.selectedEmojiText}>{userData.emoji}</Text>
+                                <View
+                                    style={[
+                                        stylesProfile.selectedEmojiItem,
+                                        { borderColor: userData.color }
+                                    ]}
+                                >
+                                    <Text
+                                        style={stylesProfile.selectedEmojiText}
+                                    >
+                                        {userData.emoji}
+                                    </Text>
                                 </View>
                             </View>
 
-                            <Text style={stylesProfile.topContentName}>{userData.firstName} {userData.lastName} ({userData.userType})</Text>
-                            <Text style={stylesProfile.topContentText}>{userData.email}</Text>
-                            <Text style={stylesProfile.topContentText}>{userData.phoneNumber}</Text>
+                            <Text style={stylesProfile.topContentName}>
+                                {userData.firstName} {userData.lastName} (
+                                {userData.userType})
+                            </Text>
+                            <Text style={stylesProfile.topContentText}>
+                                {userData.email}
+                            </Text>
+                            <Text style={stylesProfile.topContentText}>
+                                {userData.phoneNumber}
+                            </Text>
                         </View>
                     )}
+
                     <View style={stylesProfile.contentContainer}>
                         <View style={stylesProfile.buttons}>
                             <View style={stylesProfile.buttonWrapper}>
-                                <TouchableOpacity style={stylesProfile.button} onPress={handleContactSupport}>
-                                    <Text style={stylesProfile.buttonText}>Contact support</Text>
+                                <TouchableOpacity
+                                    style={stylesProfile.button}
+                                    onPress={handleContactSupport}
+                                >
+                                    <Text style={stylesProfile.buttonText}>
+                                        Contact support
+                                    </Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={stylesProfile.buttonWrapper}>
-                                <TouchableOpacity style={[stylesProfile.button, stylesProfile.logoutButton]} onPress={() => setLogoutModalVisible(true)}>
-                                    <Text style={[stylesProfile.buttonText, stylesProfile.logoutButtonText]}>Log Out</Text>
+                                <TouchableOpacity
+                                    style={[
+                                        stylesProfile.button,
+                                        stylesProfile.logoutButton
+                                    ]}
+                                    onPress={() => setLogoutModalVisible(true)}
+                                >
+                                    <Text
+                                        style={[
+                                            stylesProfile.buttonText,
+                                            stylesProfile.logoutButtonText
+                                        ]}
+                                    >
+                                        Log Out
+                                    </Text>
                                 </TouchableOpacity>
                             </View>
                             <View style={stylesProfile.buttonWrapper}>
-                                <TouchableOpacity style={[stylesProfile.button, stylesProfile.logoutButton]} onPress={() => setDeleteModalVisible(true)}>
-                                    <Text style={[stylesProfile.buttonText, stylesProfile.logoutButtonText]}>Delete Account</Text>
+                                <TouchableOpacity
+                                    style={[
+                                        stylesProfile.button,
+                                        stylesProfile.logoutButton
+                                    ]}
+                                    onPress={() => setDeleteModalVisible(true)}
+                                >
+                                    <Text
+                                        style={[
+                                            stylesProfile.buttonText,
+                                            stylesProfile.logoutButtonText
+                                        ]}
+                                    >
+                                        Delete Account
+                                    </Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -107,7 +180,9 @@ export default function ProfileSupporterScreen({ navigation }) {
             </SafeAreaView>
 
             {(logoutModalVisible || deleteModalVisible) && (
-                <Animated.View style={[styles.overlay, { opacity: overlayOpacity }]} />
+                <Animated.View
+                    style={[styles.overlay, { opacity: overlayOpacity }]}
+                />
             )}
 
             <LogoutModal
@@ -130,22 +205,22 @@ const stylesProfile = StyleSheet.create({
         fontSize: 18,
         fontFamily: 'poppins-medium',
         lineHeight: 22,
-        marginBottom: 5,
+        marginBottom: 5
     },
     container: {
-        justifyContent: 'space-between',
+        justifyContent: 'space-between'
     },
     contentContainer: {
         flex: 1,
         width: '100%',
-        justifyContent: 'space-between',
+        justifyContent: 'space-between'
     },
     topContent: {
         paddingTop: 10,
         justifyContent: 'center',
         alignItems: 'center',
         paddingBottom: 30,
-        gap: 3,
+        gap: 3
     },
     topContentName: {
         fontSize: 20,
@@ -157,37 +232,37 @@ const stylesProfile = StyleSheet.create({
         fontSize: 13,
         fontFamily: 'poppins-medium',
         lineHeight: 18,
-        color: '#747474',
+        color: '#747474'
     },
     buttons: {
         width: '100%',
-        paddingTop: 10,
+        paddingTop: 10
     },
     buttonWrapper: {
         paddingHorizontal: 25,
         borderTopColor: '#E5E5E5',
-        borderTopWidth: 1,
+        borderTopWidth: 1
     },
     button: {
         width: '100%',
         flexDirection: 'row',
-        paddingVertical: 25,
+        paddingVertical: 25
     },
     buttonText: {
         color: '#000',
         fontSize: 15,
         fontFamily: 'poppins-regular',
-        lineHeight: 18,
+        lineHeight: 18
     },
     logoutButtonText: {
-        color: '#FF7070',
+        color: '#FF7070'
     },
     linksWrapper: {
         paddingHorizontal: 25,
-        paddingVertical: 15,
+        paddingVertical: 15
     },
     link: {
-        paddingVertical: 5,
+        paddingVertical: 5
     },
     linkText: {
         color: '#000',
@@ -201,7 +276,7 @@ const stylesProfile = StyleSheet.create({
         justifyContent: 'flex-end',
         width: '100%',
         paddingVertical: 20,
-        paddingHorizontal: 25,
+        paddingHorizontal: 25
     },
     illustration: {
         width: 130,
@@ -215,11 +290,11 @@ const stylesProfile = StyleSheet.create({
         borderWidth: 2,
         borderColor: '#000',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     selectedEmojiText: {
-        fontSize: (Platform.OS === 'android') ? 60 : 65,
+        fontSize: Platform.OS === 'android' ? 60 : 65,
         textAlign: 'center',
-        lineHeight: (Platform.OS === 'android') ? 70 : 75,
-    },
+        lineHeight: Platform.OS === 'android' ? 70 : 75
+    }
 });
