5fbcf36f5ff0b60086be5b1a63cf8a194c322555
Roland Füst <roland.fust@cubicfox.com>
2024-05-28 11:30:42 +0200

    fix: my task element drawer button

--- components/MyTask.js
+++ components/MyTask.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Platform, Image, Modal, Linking } from "react-native";
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Platform,
+    Image,
+    Modal,
+    Linking
+} from 'react-native';
 import { modalServices } from '../data/ModalServices';
 import AddToCalICon from '../assets/icons/add-to-cal-icon.svg';
 import { getAccessToken } from '../authStorage';
@@ -8,7 +17,19 @@ import CheckIcon from '../assets/icons/check-medium-icon.svg';
 import { useToast } from 'react-native-toast-notifications';
 import * as Calendar from 'expo-calendar';
 
-export default function Task({ task, title, firstName, lastName, startTime, endTime, taskModal, onTaskItemClick, category, isCheckbox, onTaskStatusChange }) {
+export default function Task({
+    task,
+    title,
+    firstName,
+    lastName,
+    startTime,
+    endTime,
+    taskModal,
+    onTaskItemClick,
+    category,
+    isCheckbox,
+    onTaskStatusChange
+}) {
     const [isChecked, setIsChecked] = useState(task.status === 'done');
     const toast = useToast();
 
@@ -39,12 +60,20 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
     };
 
     const formatDate = (date) => {
-        const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+        const options = {
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        };
         return new Date(date).toLocaleDateString('en-US', options);
     };
 
     const findIcon = () => {
-        const service = modalServices.find(service => service.title === category);
+        const service = modalServices.find(
+            (service) => service.title === category
+        );
         return service ? service.icon : null;
     };
 
@@ -56,7 +85,8 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
     };
 
     const [calendarPermission] = Calendar.useCalendarPermissions();
-    const [calendarPermissionNeeded, setCalendarPermissionNeeded] = useState(false);
+    const [calendarPermissionNeeded, setCalendarPermissionNeeded] =
+        useState(false);
 
     const handleOpenCalendar = async () => {
         // console.log('CALENDAR PERMISSION:', calendarPermission);
@@ -65,11 +95,19 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
             return;
         }
 
-        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+        const calendars = await Calendar.getCalendarsAsync(
+            Calendar.EntityTypes.EVENT
+        );
         console.log('CALENDARS:', calendars);
         const defaultCalendar = Platform.select({
-            ios: calendars.find(cal => cal.allowsModifications /* && cal.source.name === 'Default' */),
-            android: calendars.find(cal => cal.accessLevel === "owner" && cal.name === cal.ownerAccount),
+            ios: calendars.find(
+                (cal) =>
+                    cal.allowsModifications /* && cal.source.name === 'Default' */
+            ),
+            android: calendars.find(
+                (cal) =>
+                    cal.accessLevel === 'owner' && cal.name === cal.ownerAccount
+            )
         });
 
         if (!defaultCalendar) {
@@ -82,7 +120,7 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
             title: task.title,
             startDate: new Date(task.startDateTime),
             endDate: new Date(task.endDateTime),
-            notes: task.description,
+            notes: task.description
         };
 
         await Calendar.createEventAsync(defaultCalendar.id, event)
@@ -94,7 +132,7 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
                 console.error('Error adding event to calendar:', error);
                 toast.show('Error adding event to calendar', { type: 'error' });
             });
-    }
+    };
 
     const handleGoToSettings = () => {
         Linking.openSettings();
@@ -103,29 +141,74 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
 
     return (
         <>
-            <TouchableOpacity style={[stylesTask.taskContainer, isChecked && stylesTask.greyedOut]} activeOpacity={0.7} onPress={handleClick}>
-                {isCheckbox &&
-                    <TouchableOpacity style={stylesTask.checkboxWrapper} onPress={handleToggleCheckbox}>
-                        <View style={isChecked ? stylesTask.checkboxChecked : stylesTask.checkbox}>
-                            {isChecked &&
-                                <View style={stylesTask.checkboxInner}>
-                                    <CheckIcon width={13} height={13} style={stylesTask.checkboxIcon} />
-                                </View>
+            <TouchableOpacity
+                style={[
+                    stylesTask.taskContainer,
+                    isChecked && stylesTask.greyedOut
+                ]}
+                activeOpacity={0.7}
+                onPress={handleClick}
+            >
+                {isCheckbox && (
+                    <TouchableOpacity
+                        style={stylesTask.checkboxWrapper}
+                        onPress={handleToggleCheckbox}
+                    >
+                        <View
+                            style={
+                                isChecked
+                                    ? stylesTask.checkboxChecked
+                                    : stylesTask.checkbox
                             }
+                        >
+                            {isChecked && (
+                                <View style={stylesTask.checkboxInner}>
+                                    <CheckIcon
+                                        width={13}
+                                        height={13}
+                                        style={stylesTask.checkboxIcon}
+                                    />
+                                </View>
+                            )}
                         </View>
                     </TouchableOpacity>
-                }
+                )}
 
                 <View style={stylesTask.taskInfoContainer}>
-                    <Text style={[stylesTask.taskTitle, isChecked ? stylesTask.textStriked : '', isChecked && stylesTask.greyedOut]}>{title}</Text>
+                    <Text
+                        style={[
+                            stylesTask.taskTitle,
+                            isChecked ? stylesTask.textStriked : '',
+                            isChecked && stylesTask.greyedOut
+                        ]}
+                    >
+                        {title}
+                    </Text>
                     {firstName && lastName && (
-                        <Text style={stylesTask.taskAssignee}>{firstName} {lastName}</Text>
+                        <Text style={stylesTask.taskAssignee}>
+                            {firstName} {lastName}
+                        </Text>
                     )}
-                    <Text style={[stylesTask.taskTime, isChecked ? stylesTask.textStriked : '', isChecked && stylesTask.greyedOut]}>{formatDateTime()}</Text>
+                    <Text
+                        style={[
+                            stylesTask.taskTime,
+                            isChecked ? stylesTask.textStriked : '',
+                            isChecked && stylesTask.greyedOut
+                        ]}
+                    >
+                        {formatDateTime()}
+                    </Text>
                 </View>
                 <View style={stylesTask.iconsWrapper}>
-                    <TouchableOpacity style={stylesTask.addToCalIconWrapper} onPress={handleOpenCalendar}>
-                        <AddToCalICon width={30} height={30} style={stylesTask.addToCalIcon} />
+                    <TouchableOpacity
+                        style={stylesTask.addToCalIconWrapper}
+                        onPress={handleOpenCalendar}
+                    >
+                        <AddToCalICon
+                            width={30}
+                            height={30}
+                            style={stylesTask.addToCalIcon}
+                        />
                     </TouchableOpacity>
                     <View style={stylesTask.serviceIconWrapper}>
                         <Image source={icon} style={stylesTask.serviceIcon} />
@@ -134,16 +217,43 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
             </TouchableOpacity>
 
             {calendarPermissionNeeded && (
-                <Modal visible={calendarPermissionNeeded} onRequestClose={() => console.log("close")} animationType='fade' transparent>
-                    <TouchableOpacity onPress={() => console.log("close")} style={stylesTask.innerModalContainer}>
+                <Modal
+                    visible={calendarPermissionNeeded}
+                    onRequestClose={() => console.log('close')}
+                    animationType="fade"
+                    transparent
+                >
+                    <TouchableOpacity
+                        onPress={() => console.log('close')}
+                        style={stylesTask.innerModalContainer}
+                    >
                         <View style={stylesTask.innerModalContent}>
                             <View style={stylesTask.innerModalTexts}>
-                                <Text style={stylesTask.innerModalTitle}>Please provide access for this app to your calendar.</Text>
-                                <Text style={stylesTask.innerModalSubtitle}>Without acces we cannot export this event to your calendar.</Text>
+                                <Text style={stylesTask.innerModalTitle}>
+                                    Please provide access for this app to your
+                                    calendar.
+                                </Text>
+                                <Text style={stylesTask.innerModalSubtitle}>
+                                    Without acces we cannot export this event to
+                                    your calendar.
+                                </Text>
                             </View>
                             <View style={stylesTask.innerModalButtons}>
-                                <TouchableOpacity style={[stylesTask.innerModalButton, stylesTask.innerModalButtonRed]} onPress={handleGoToSettings}>
-                                    <Text style={[stylesTask.innerModalButtonText, stylesTask.innerModalButtonRedText]}>Go to Settings</Text>
+                                <TouchableOpacity
+                                    style={[
+                                        stylesTask.innerModalButton,
+                                        stylesTask.innerModalButtonRed
+                                    ]}
+                                    onPress={handleGoToSettings}
+                                >
+                                    <Text
+                                        style={[
+                                            stylesTask.innerModalButtonText,
+                                            stylesTask.innerModalButtonRedText
+                                        ]}
+                                    >
+                                        Go to Settings
+                                    </Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -152,7 +262,7 @@ export default function Task({ task, title, firstName, lastName, startTime, endT
             )}
         </>
     );
-};
+}
 
 const stylesTask = StyleSheet.create({
     taskContainer: {
@@ -163,68 +273,74 @@ const stylesTask = StyleSheet.create({
         paddingVertical: 15,
         backgroundColor: '#fff',
         borderRadius: 20,
-        shadowColor: (Platform.OS === 'android') ? 'rgba(0,0,0,0.5)' : '#000',
+        shadowColor: Platform.OS === 'android' ? 'rgba(0,0,0,0.5)' : '#000',
         shadowOffset: {
             width: 0,
-            height: 4,
+            height: 4
         },
         shadowOpacity: 0.09,
-        shadowRadius: 8.00,
-        elevation: 12,
+        shadowRadius: 8.0,
+        elevation: 12
+    },
+    innerModalButtonRedText: {
+        color: '#fff'
+    },
+    innerModalButtonRed: {
+        backgroundColor: '#FF7070'
     },
     taskInfoContainer: {
         flexShrink: 1,
         gap: 3,
         minHeight: 50,
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     taskTitle: {
         color: '#000',
         fontSize: 14,
         fontFamily: 'poppins-regular',
         lineHeight: 16,
-        marginBottom: 2,
+        marginBottom: 2
     },
     taskAssignee: {
         color: '#747474',
         fontSize: 12,
         fontFamily: 'poppins-regular',
-        lineHeight: 14,
+        lineHeight: 14
     },
     taskTime: {
         color: '#9F9F9F',
         fontSize: 12,
         fontFamily: 'poppins-light',
-        lineHeight: 14,
+        lineHeight: 14
     },
     iconsWrapper: {
         flexDirection: 'row',
         gap: 5,
-        paddingRight: 10,
+        paddingRight: 10
     },
     serviceIconWrapper: {
         width: 45,
         height: 45,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     serviceIcon: {
         width: 35,
         height: 35,
-        resizeMode: 'contain',
+        resizeMode: 'contain'
     },
     addToCalIconWrapper: {
         width: 45,
         height: 45,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     addToCalIcon: {
         marginLeft: -5,
-        marginBottom: -5,
+        marginBottom: -5
     },
     textStriked: {
-        textDecorationLine: 'line-through',
+        textDecorationLine: 'line-through'
     },
     checkboxWrapper: {
         height: '100%',
@@ -233,7 +349,7 @@ const stylesTask = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         borderTopRightRadius: 20,
-        borderBottomRightRadius: 20,
+        borderBottomRightRadius: 20
     },
     checkbox: {
         borderWidth: 1,
@@ -243,10 +359,10 @@ const stylesTask = StyleSheet.create({
         borderRadius: 9,
         position: 'relative',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     checkboxChecked: {
-        borderColor: '#6AAA5F',
+        borderColor: '#6AAA5F'
     },
     checkboxInner: {
         backgroundColor: '#6AAA5F',
@@ -254,31 +370,31 @@ const stylesTask = StyleSheet.create({
         alignItems: 'center',
         width: 20,
         height: 20,
-        borderRadius: 10,
+        borderRadius: 10
     },
     checkboxIcon: {
         resizeMode: 'contain',
-        color: '#fff',
+        color: '#fff'
     },
     greyedOut: {
-        color: '#B0B0B0',
+        color: '#B0B0B0'
     },
     innerModalContainer: {
         ...StyleSheet.absoluteFillObject,
         backgroundColor: 'rgba(0,0,0,0.5)',
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     innerModalContent: {
         backgroundColor: '#fff',
         borderRadius: 20,
         maxWidth: 350,
         paddingHorizontal: 20,
-        paddingVertical: 30,
+        paddingVertical: 30
     },
     innerModalTexts: {
-        marginBottom: 20,
+        marginBottom: 20
     },
     innerModalTitle: {
         color: '#000',
@@ -293,12 +409,12 @@ const stylesTask = StyleSheet.create({
         fontSize: 12,
         fontFamily: 'poppins-regular',
         lineHeight: 16,
-        textAlign: 'center',
+        textAlign: 'center'
     },
     innerModalButtons: {
         flexDirection: 'row',
         gap: 10,
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     innerModalButton: {
         alignItems: 'center',
@@ -306,30 +422,30 @@ const stylesTask = StyleSheet.create({
         paddingHorizontal: 20,
         paddingVertical: 10,
         borderRadius: 16,
-        shadowColor: (Platform.OS === 'android') ? 'rgba(0,0,0,0.5)' : '#000',
+        shadowColor: Platform.OS === 'android' ? 'rgba(0,0,0,0.5)' : '#000',
         shadowOffset: {
             width: 0,
-            height: 3,
+            height: 3
         },
         shadowOpacity: 0.1,
         shadowRadius: 6,
-        elevation: 8,
+        elevation: 8
     },
     innerModalButtonGreen: {
-        backgroundColor: '#1C4837',
+        backgroundColor: '#1C4837'
     },
     innerModalButtonWhite: {
-        backgroundColor: '#fff',
+        backgroundColor: '#fff'
     },
     innerModalButtonText: {
         fontSize: 14,
         fontFamily: 'poppins-medium',
-        lineHeight: 18,
+        lineHeight: 18
     },
     innerModalButtonGreenText: {
-        color: '#fff',
+        color: '#fff'
     },
     innerModalButtonWhiteText: {
-        color: '#000',
-    },
+        color: '#000'
+    }
 });
