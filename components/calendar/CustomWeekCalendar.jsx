570c2c02c53c43f222e79ef3709e6d17a0220baf
Roland Füst <roland.fust@cubicfox.com>
2024-06-06 09:29:42 +0200

    fix: month text in weekly view

--- components/calendar/CustomWeekCalendar.jsx
+++ components/calendar/CustomWeekCalendar.jsx
@@ -1,10 +1,9 @@
 import React, { useRef, useCallback, useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {
     ExpandableCalendar,
     AgendaList,
-    CalendarProvider,
-    WeekCalendar
+    CalendarProvider
 } from 'react-native-calendars';
 import { CALENDAR_THEME_SETTINGS } from '../../constants/calendar';
 import AgendaItem from './AgendaItem';
@@ -19,24 +18,17 @@ export default function CustomWeekCalendar({
     setTaskModalVisible,
     handleTaskStatusChange
 }) {
-    const weekView = true;
+    //const weekView = false;
 
     const agendaItems = generateAgendaItems(tasks);
 
     const marked = useRef(getMarkedDates(agendaItems));
 
-    const [currentMonth, setCurrentMonth] = useState('');
     const [selectedDate, setSelectedDate] = useState('');
 
     const onDayPress = (day) => {
         setSelectedDate(day.dateString);
     };
-
-    useEffect(() => {
-        const todayDate = new Date();
-        const month = todayDate.toLocaleString('default', { month: 'long' });
-        setCurrentMonth(month);
-    }, []);
 
     const renderItem = useCallback(({ item }) => {
         return (
@@ -56,38 +48,22 @@ export default function CustomWeekCalendar({
                 showTodayButton
                 theme={CALENDAR_THEME_SETTINGS}
             >
-                <View style={{ paddingHorizontal: 25, paddingVertical: 5 }}>
-                    <Text style={styles.monthHeading}>{currentMonth}</Text>
-                </View>
-                {weekView ? (
-                    <WeekCalendar
-                        firstDay={1}
-                        style={styles.calendar}
-                        theme={CALENDAR_THEME_SETTINGS}
-                        markedDates={{
-                            ...marked.current,
-                            [selectedDate]: {
-                                selected: true,
-                                selectedColor: '#1C4837'
-                            }
-                        }}
-                        staticHeader={false}
-                        showWeekNumbers
-                        // onVisibleMonthsChange={(months) =>
-                        //     handleMonthChange(months)
+                <ExpandableCalendar
+                    hideArrows
+                    theme={CALENDAR_THEME_SETTINGS}
+                    firstDay={1}
+                    markedDates={{
+                        ...marked.current
+                        // [selectedDate]: {
+                        //     selected: true,
+                        //     selectedColor: '#1C4837',
+                        //     dotColor: 'white'
                         // }
-                        //onMonthChange={(month) => handleMonthChange(month)}
-                        //numberOfDays={7}
-                        onDayPress={onDayPress}
-                    />
-                ) : (
-                    <ExpandableCalendar
-                        hideArrows
-                        theme={CALENDAR_THEME_SETTINGS}
-                        firstDay={1}
-                        markedDates={marked.current}
-                    />
-                )}
+                    }}
+                    hideKnob
+                    onDayPress={onDayPress}
+                />
+
                 <AgendaList
                     sections={agendaItems}
                     renderItem={renderItem}
