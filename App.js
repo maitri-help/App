4d42b619cc786056225893d8d1bd64d4b84953b1
Roland Füst <roland.fust@cubicfox.com>
2024-06-25 13:19:57 +0200

    feat: add logrocket idintify

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './components/Navigators/MainNavigator';
 import { Providers } from './providers/Providers';
 import LogRocket from '@logrocket/react-native';
+import { LOGROCKET_APP_ID } from './constants/config';
 
-LogRocket.init('ooup7k/maitri');
+LogRocket.init(LOGROCKET_APP_ID);
 
 export default function App() {
     return (
--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,8 @@
 import {
     EXPO_PUBLIC_API_URL,
     EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
-    EXPO_PUBLIC_GEOLOCATION_API_KEY
+    EXPO_PUBLIC_GEOLOCATION_API_KEY,
+    EXPO_PUBLIC_LOGROCKET_APP_ID
 } from '@env';
 
 export const API_URL = EXPO_PUBLIC_API_URL || '';
@@ -10,3 +11,4 @@ export const GEOLOCATION_API_KEY = EXPO_PUBLIC_GEOLOCATION_API_KEY || '';
 export const GOOGLE_STORE_URL =
     'https://play.google.com/store/apps/details?id=com.Maitri.MaitriApp';
 export const APPLE_STORE_URL = 'https://apps.apple.com/app/id6503946923';
+export const LOGROCKET_APP_ID = EXPO_PUBLIC_LOGROCKET_APP_ID | '';
